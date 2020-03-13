f7824ff9ded47ac3d51ab2680999fa12122acfdb
Salint <41103301+Salint@users.noreply.github.com>
2020-03-13 14:13:32 +0200

    Updated creator.js

--- cmds/creator.js
+++ cmds/creator.js
@@ -1,15 +1,15 @@
 const app = require("../bot.js");
-let client = app.client;
-let Discord = app.Discord;
-let config = app.config;
+const client = app.client;
+const Discord = app.Discord;
+const config = app.config;
         
 function run(msg, args) {
      // code here will run
      let embed = new Discord.RichEmbed()
-     .setTitle('Occult Waifu#1659')
+     .const('Occult Waifu#1659')
      .setColor([255, 0, 0])
      .setDescription('The creator of Karen Bot and other bots.')
-     .addField('Bio', 'A musician that has no life.')
+     .addField('Auto-Biography', 'A musician that has no life.')
      .addField('Creations', 'Occult Waifu has made Karen Bot, Kuzey Bot, Nice Bot and Kara Bot')
      //.addField('')
      .setFooter('Author - Occult Waifu#1659', 'https://i.imgur.com/yxq3XXl.jpg')
